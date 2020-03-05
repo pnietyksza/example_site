cc36c5fed05771b36cb7debbc6a12bc2c250f584
pnietyksza <patryk.nietyksza@gmail.com>
2020-03-05 20:06:21 +0100

    Add new

---
+++ style/js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+  $('.myButton').click( function()
+    {
+      window.scrollBy(0,780);
+    }
+  );
+});
---
+++ testjs.js
@@ -0,0 +1,9 @@
+var li = document.getElementsByTagName('li');
+
+
+var i = 0;
+
+while (i<4) {
+  alert(li[i].innerHTML);
+  i++;
+}
